a137d212aa59312a2c4aefaf6b587ff7625f7fbc
Baris Sertkaya <sertkaya.baris@gmail.com>
2009-02-11 14:35:19 +0000

    Initial version. Constants for OClib

---
+++ src/de/tudresden/inf/tcs/oclib/Constants.java
@@ -0,0 +1,8 @@
+package de.tudresden.inf.tcs.oclib;
+
+public interface Constants {
+
+	// Type of individual object description update (whether it is after a modification, or after an undo)
+	static final int AFTER_MODIFICATION = 0;
+	static final int AFTER_UNDO = 1;
+}
