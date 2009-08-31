453c4ef7e42234373274ef16767384c3325e46b5
Baris Sertkaya <sertkaya.baris@gmail.com>
2009-08-31 14:46:32 +0000

    added reasoner id for cel and prefix for complement concepts

--- src/de/tudresden/inf/tcs/oclib/Constants.java
+++ src/de/tudresden/inf/tcs/oclib/Constants.java
@@ -5,4 +5,10 @@ public interface Constants {
 	// Type of individual object description update (whether it is after a modification, or after an undo)
 	static final int AFTER_MODIFICATION = 0;
 	static final int AFTER_UNDO = 1;
+	
+	// The ID of the CEL reasoner. Looks like it contains redundancies, but OK. CEL identifies it like this
+	static final String CEL_REASONER_ID="de.tudresden.inf.lat.cel.de.tudresden.inf.lat.cel.protege.CelReasonerFactory";
+	
+	// The prefix that is used for name of the complement concept added to an EL context
+	static final String EL_COMPLEMENT_CONCEPT_PREFIX="complementOf-";
 }
